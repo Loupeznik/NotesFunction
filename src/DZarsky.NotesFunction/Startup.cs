357c8f216ae4377f58b08ce2ccb9501fbc7648d6
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-02-05 10:16:03 +0100

    add common library

---
+++ src/DZarsky.NotesFunction/Startup.cs
@@ -0,0 +1,25 @@
+﻿using DZarsky.CommonLibraries.AzureFunctions.Infrastructure;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(DZarsky.NotesFunction.Startup))]
+namespace DZarsky.NotesFunction
+{
+    internal class Startup : FunctionsStartup
+    {
+        private static readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .AddUserSecrets<Startup>(true)
+            .Build();
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.AddCommonFunctionServices(_configuration);
+
+        }
+    }
+}
